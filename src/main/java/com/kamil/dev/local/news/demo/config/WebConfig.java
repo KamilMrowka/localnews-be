cbd98a920cdaccf6f9b32dd5b43060cc3611b30f
Kamil Mrówka <kamil31245@gmail.com>
2024-10-25 01:08:43 +0200

    webconfig for cors config

---
+++ src/main/java/com/kamil/dev/local/news/demo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.kamil.dev.local.news.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")  // Apply CORS to all APIs under /api/
+                .allowedOrigins("http://16.170.178.215", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
