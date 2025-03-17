f3e0055d26ab398fb9d2f1fa53512d8c17acdd4b
Kamil Mrówka <kamil31245@gmail.com>
2025-03-17 12:19:19 +0100

    Take the cors config from the env variables

--- src/main/java/com/kamil/dev/local/news/demo/config/WebConfig.java
+++ src/main/java/com/kamil/dev/local/news/demo/config/WebConfig.java
@@ -1,16 +1,25 @@
 package com.kamil.dev.local.news.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed.origins}")
+    private String corsAllowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // Apply CORS to all APIs under /api/
-                .allowedOrigins("http://13.48.254.174", "http://localhost:5173")
+        List<String> allowedOrigins = Arrays.asList(corsAllowedOrigins.split(","));
+
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
